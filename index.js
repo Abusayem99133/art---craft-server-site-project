ec331bbab109436ca6fd303894832dc35129324f
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-04-27 14:42:10 +0600

    mongodb config install and connected to  backend server

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
+
 // middleware
-app.use(cors());
+app.use(cors())
 app.use(express.json())
 
+// paintingProject
+// DWnatRZgTKByidXl
 
-app.get('/', (req, res) =>{
-    res.send('Craft item making server is running')
+console.log(process.env.DB_USER);
+
+
+
+const uri = "mongodb+srv://paintingProject:DWnatRZgTKByidXl@cluster0.ddlv3rx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const uri = "mongodb+srv://paintingProject:<password>@cluster0.ddlv3rx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/',(req, res)=>{
+    res.send('craft making to server')
 })
-app.listen(port, () =>{
-    console.log(`craft item server is running port ${port}`);
-    
+app.listen(port, ()=>{
+    console.log(`craft server is runnig : ${port}`);
 })
